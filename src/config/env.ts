f09c47028c1a50b09dca4d6fa6cc71b8469319da
Andres Vega <elb.andres8@gmail.com>
2024-05-29 19:49:08 -0500

    First commit

---
+++ src/config/env.ts
@@ -0,0 +1,27 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+type EnvVars = {
+  PORT: number;
+  DATABASE_URL: string;
+};
+
+const envSchema = joi
+  .object({
+    PORT: joi.number().required(),
+    DATABASE_URL: joi.string().required(),
+  })
+  .unknown(true);
+
+const { error, value } = envSchema.validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error}`);
+}
+
+const envVars: EnvVars = value;
+
+export const envs = {
+  port: envVars.PORT,
+  databaseUrl: envVars.DATABASE_URL,
+};
